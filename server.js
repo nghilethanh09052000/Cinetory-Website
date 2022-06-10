88533f163441975cdb471c7aa27b0e7c77074c08
Your Name <nghilt19411@gmail.com>
2022-06-10 11:45:49 +0700

    add backend

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const api = require('./src/api')
 var cors = require('cors')
-
 const app = express();
-app.use(cors())
-var port = process.env.PORT || 3000;
+const apiRoutes = require('./routes/api-routes')
+const mailRoutes = require('./routes/mail-routes')
 
-app.get('/',(req,res)=>{
-    res.json(api)
-})
+app.use(cors())
+var port = process.env.PORT || 3003;
+
+app.use(apiRoutes)
+app.use(mailRoutes)
+
 app.listen(port)
